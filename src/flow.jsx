8d94aef2b213815bc6783993d5f7d6b1a57ae357
jacob whitford-bender <jacob@xmentium.com>
2023-04-30 13:09:21 -0600

    feat: EditorDialog

--- src/flow.jsx
+++ src/flow.jsx
@@ -1,4 +1,4 @@
-import {useCallback, createContext, useContext, useRef} from 'react'
+import {useCallback, useState, createContext, useContext, useRef, Fragment} from 'react'
 import ReactFlow, {
 	Controls,
 	Background,
@@ -12,6 +12,7 @@ import {predicates, classes, optionsByClass} from './rdf'
 import ReactMarkdown from 'react-markdown'
 import {v1 as uuid} from 'uuid'
 
+const DialogContext = createContext({})
 const ChangeContext = createContext({})
 const defix = s => s.replace(/^\w+:/, '')
 
@@ -69,23 +70,38 @@ const IbisPropertyEdge = props => {
 
 const SILVER_RATIO = 2.41421
 
+const EditorDialog = props => {
+	const {node, onChange} = props
+	const {edges, nodes} = useContext(ChangeContext)
+	if (!node) return null
+	const {id, data} = node
+	const myEdge = edges.find(e => e.target === id)
+	const mySource = nodes.find(n => n.id === myEdge?.source)
+	const permissableClasses = Object.keys(optionsByClass[mySource?.data.type] ?? {})
+	return (
+		<form style={{display: 'grid', gridTemplateColumns: `1fr ${SILVER_RATIO}fr`}} onSubmit={e => onChange(new FormData(e.target))} method='dialog'>
+			<label htmlFor={`label-${id}`}>
+				Label
+			</label>
+			<input id={`label-${id}`} defaultValue={data.label} name='label'/>
+			<label htmlFor={`type-${id}`}>
+				Type
+			</label>
+			<IbisSelect id={`type-${id}`} defaultValue={data.type} name="type" >
+				{permissableClasses.length ? permissableClasses : classes}
+			</IbisSelect>
+			<button onClick={e => {
+				e.preventDefault()
+				onChange(undefined)
+			}}>cancel</button>
+			<button type='submit'>submit</button>
+		</form>
+	)
+}
+
 const IbisClassNode = ({data, id, ...props}) => {
-	const {setNodes, nodes, setEdges, edges} = useContext(ChangeContext)
-	const changeData = newData => {
-		setNodes(nodes => nodes.map(
-			n => n.id === id ? ({
-				...n,
-				data: {...n.data, ...newData},
-			}) : n
-		))
-		if (newData.type) {
-			setEdges(edges => edges.map(
-				e => e.source === id ? {...e, data: {...e.data, sourceType: newData.type}}
-				:    e.target === id ? {...e, data: {...e.data, targetType: newData.type}}
-				:                    e
-			))
-		}
-	}
+	const {setNodes, nodes, edges, changeNodeData} = useContext(ChangeContext)
+	const {handleOpenDialog} = useContext(DialogContext)
 	const myEdge = edges.find(e => e.target === id)
 	const mySource = nodes.find(n => n.id === myEdge?.source)
 	const permissableClasses = Object.keys(optionsByClass[mySource?.data.type] ?? {})
@@ -101,31 +117,35 @@ const IbisClassNode = ({data, id, ...props}) => {
 			</div>
 			<NodeToolbar>
 				<div style={{display: 'grid', gridTemplateColumns: `1fr ${SILVER_RATIO}fr`}}>
-					<label htmlFor={`title-${id}`}>
-						Title
+					<label htmlFor={`label-${id}`}>
+						Label
 					</label>
-					<input id={`title-${id}`} value={data.label} onChange={e => {
+					<input id={`label-${id}`} value={data.label} name='label' onChange={e => {
 						const {value} = e.target
-						changeData({label: value})
+						changeNodeData(id, {label: value})
 					}}/>
 					<label htmlFor={`type-${id}`}>
 						Type
 					</label>
-					<IbisSelect
-						id={`type-${id}`} value={data.type} name="class" onChange={e => {
+					<IbisSelect id={`type-${id}`} value={data.type} name='type' onChange={e => {
 						const {value} = e.target
-						changeData({type: value})
+						changeNodeData(id, {type: value})
 					}}>
 						{permissableClasses.length ? permissableClasses : classes}
 					</IbisSelect>
 					<div></div>
-					<button
-						disabled={data.type === 'ibis:Issue' && nodes.filter(n => n.data.type === 'ibis:Issue').length < 2}
-						style={{color: '#dd0000'}}
-						onClick={handleDelete}
-					>
-						delete
-					</button>
+					<div style={{display: 'grid', gridTemplateColumns: `1fr ${SILVER_RATIO}fr`}}>
+						<button
+							disabled={data.type === 'ibis:Issue' && nodes.filter(n => n.data.type === 'ibis:Issue').length < 2}
+							style={{color: '#dd0000'}}
+							onClick={handleDelete}
+						>
+							delete
+						</button>
+						<button onClick={() => handleOpenDialog(id)}>
+							open editor dialog
+						</button>
+					</div>
 				</div>
 			</NodeToolbar>
 		</>
@@ -152,7 +172,6 @@ const defaultsBySource = ({data = {}} = {}) => ({
 }[data.type] ?? {edge: 'ibis:suggests', node: 'ibis:Argument', direction: {}})
 
 const getId = () => uuid()
-const {Provider} = ChangeContext
 export function Edge(fromNode, toNode, {type} = {}) {
 	const {edge, direction} = defaultsBySource(fromNode)
 	Object.assign(this, {
@@ -179,6 +198,33 @@ export function Node(fromNode, {position}) {
 	};
 	if (!fromNode) return this
 	this.edge = new Edge(fromNode, this.node);
+}
+const DialogProvider = props => {
+	const {children, nodes, changeNodeData} = props
+	const dialog = useRef()
+	const [id, setId] = useState()
+	const activeNode = nodes.find(n => n.id === id)
+	const {Provider} = DialogContext
+	const handleChange = newData => {
+		if (newData) changeNodeData(id, Object.fromEntries(newData.entries()))
+		setId(undefined)
+		dialog.current.close()
+	}
+	return (
+		<Fragment>
+			<dialog ref={dialog}>
+				<EditorDialog onChange={handleChange} node={activeNode}></EditorDialog>
+			</dialog>
+			<Provider value={{
+				handleOpenDialog: newId => {
+					setId(newId)
+					dialog.current.showModal()
+				},
+			}}>
+				{children}
+			</Provider>
+		</Fragment>
+	)
 }
 export const FlowChartLol = props => {
 	const {setEdges, setNodes, children, edges, nodes, ...rest} = props
@@ -221,24 +267,42 @@ export const FlowChartLol = props => {
 		},
 		[setEdges, nodes]
 	)
+	const {Provider} = ChangeContext
+	const changeNodeData = (id, newData) => {
+		setNodes(nodes => nodes.map(
+			n => n.id === id ? ({
+				...n,
+				data: {...n.data, ...newData},
+			}) : n
+		))
+		if (newData.type) {
+			setEdges(edges => edges.map(
+				e => e.source === id ? {...e, data: {...e.data, sourceType: newData.type}}
+				:    e.target === id ? {...e, data: {...e.data, targetType: newData.type}}
+				:                      e
+			))
+		}
+	}
 	return (
-		<Provider value={{edges, nodes, setEdges, setNodes}}>
-			<div style={{height: '100%', width: '100%'}} ref={reactFlowWrapper}>
-				<ReactFlow {...{
-					...rest,
-					edges,
-					nodes,
-					onConnectStart,
-					onConnectEnd,
-					onConnect,
-					nodeTypes,
-					edgeTypes,
-				}}>
-					{children}
-					<Background variant='lines'></Background>
-					<Controls></Controls>
-				</ReactFlow>
-			</div>
+		<Provider value={{edges, nodes, setEdges, setNodes, changeNodeData}}>
+			<DialogProvider {...{nodes, changeNodeData}}>
+				<div style={{height: '100%', width: '100%'}} ref={reactFlowWrapper}>
+					<ReactFlow {...{
+						...rest,
+						edges,
+						nodes,
+						onConnectStart,
+						onConnectEnd,
+						onConnect,
+						nodeTypes,
+						edgeTypes,
+					}}>
+						{children}
+						<Background variant='lines'></Background>
+						<Controls></Controls>
+					</ReactFlow>
+				</div>
+			</DialogProvider>
 		</Provider>
 	)
 }
